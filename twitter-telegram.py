e0992b8aa1b4712b3b929dfb03f9085ff9ae55c4
normaluser <ebarbacil33@gmail.com>
2020-09-15 13:52:24 -0300

    connection error logs

--- twitter-telegram.py
+++ twitter-telegram.py
@@ -3,6 +3,7 @@ import telepot
 import tweepy
 import logging 
 import config
+import urllib3, requests
 from IPython import embed
 from socket import error as SocketError
 import errno
@@ -19,14 +20,17 @@ def telegram_alert(tweet, bot):
 
 def setup():
     token = config.token
-    bot = telepot.Bot(token)
-    logging.info(bot.getMe())
-    return bot
-
-if __name__ == "__main__":
     try:
+        bot = telepot.Bot(token)
+        logging.info(bot.getMe())
+        return bot
+    except requests.exceptions.ReadTimeout as e:
+        logging.error("Read Time Out in Telepot")
+        time.sleep(30)
+        setup()
+
+def main(): 
         logging.basicConfig(filename='telegram-twitter-bot.log', level=logging.INFO)
-        bot = setup()
         auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
         auth.set_access_token(config.access_token, config.access_token_secret)
         api = tweepy.API(auth)
@@ -34,14 +38,34 @@ if __name__ == "__main__":
         ref_tweet = tweets[0]
         logging.info("Ref tweet text: "  + ref_tweet.text)
         logging.info("Ref tweet id: " + str(ref_tweet.id))
-
         while 1:
             time.sleep(config.sleep)
-            tweets = api.user_timeline(config.user)
-            last_tweet = tweets[0]
-            if ref_tweet != last_tweet:
-                telegram_alert(last_tweet, bot)
-                ref_tweet = last_tweet
-            pass
+            tweets = []
+            try: 
+                tweets = api.user_timeline(config.user)
+                last_tweet = tweets[0]
+                embed()
+                if ref_tweet != last_tweet:
+                    bot = setup()
+                    telegram_alert(last_tweet, bot)
+                    ref_tweet = last_tweet
+
+            except tweepy.error.TweepError as e:
+                logging.info("ConnectionError in while")
+                continue
+            except urllib3.exceptions.ProtocolError as e:
+                logging.error("Protocol Error")
+                continue
+    
+
+if __name__ == "__main__":
+    try:
+        main()
+
+    except tweepy.error.TweepError as e:
+        logging.info("ConnectionError in  ")
+        time.sleep(60)
+        main()
     except Exception as e:
+        embed()
         logging.error(str(e))
